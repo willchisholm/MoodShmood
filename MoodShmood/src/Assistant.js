0eea5f3bf190df7a39b1d6499b6c17aac705b946
Will Chisholm <william.f.chisholm.iii.20@dartmouth.edu>
2019-05-27 00:54:50 -0400

    FUCKING DONE, KAHOOT'S A BALLER

--- MoodShmood/src/Assistant.js
+++ MoodShmood/src/Assistant.js
@@ -1,39 +1,38 @@
-// import base64 from 'react-native-base64';
-// import {
-//   USERNAME, PASSWORD, URL, SKILL_ID,
-// } from 'react-native-dotenv';
+/* eslint-disable no-undef */
+import base64 from 'react-native-base64';
 
-// // Watson Assistant API documentation:
-// // https://console.bluemix.net/apidocs/assistant
-// MessageRequest = (input, context = {}) => {
-//   const body = {
-//     alternate_intents: true,
-//     input: {
-//       text: input,
-//     },
-//   };
-//   if (context) {
-//     body.context = context;
-//   }
-//   return fetch(`${URL}/v1/workspaces/${SKILL_ID}/message?version=2018-09-20`, {
-//     method: 'POST',
-//     headers: {
-//       Authorization: `Basic ${base64.encode(`${USERNAME}:${PASSWORD}`)}`,
-//       Accept: 'application/json',
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(body),
-//   })
-//     .then(response => response.json())
-//     .then((responseJson) => {
-//       // console.log(responseJson);
-//       return responseJson;
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//     });
-// };
+// Watson Assistant API documentation:
+// https://console.bluemix.net/apidocs/assistant
+MessageRequest = (input, context = {}) => {
+  const body = {
+    alternate_intents: true,
+    input: {
+      text: input,
+    },
+  };
+  if (context) {
+    body.context = context;
+  }
+  return fetch('https://gateway.watsonplatform.net/assistant/api/v1/workspaces/e3d15e39-9c55-4ec9-9c6b-72726b9ec7d1/message?version=2018-09-20', {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${base64.encode('apikey:B65cRu3Vi-J4qbhdC0KB_QfXJQ7eIxVt1Cs_xlEyr9jE')}`,
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  });
+  // .then((response) => { response.json(); });
+  // .then((responseJson) => {
 
-// module.exports = {
-//   MessageRequest,
-// };
+  //   console.log(responseJson);
+  //   return responseJson;
+  // })
+  // .catch((error) => {
+  //   console.error(error);
+  // });
+};
+
+module.exports = {
+  MessageRequest,
+};
--- MoodShmood/src/chat.js
+++ MoodShmood/src/chat.js
@@ -1,100 +1,168 @@
-/* eslint-disable camelcase */
+/* eslint-disable react/no-access-state-in-setstate */
+/* eslint-disable react/sort-comp */
 /* eslint-disable react/destructuring-assignment */
-// adapted from https://github.com/FaridSafi/react-native-gifted-chat
+// /* eslint-disable camelcase */
+// /* eslint-disable react/destructuring-assignment */
+// // adapted from https://github.com/FaridSafi/react-native-gifted-chat
+
+// import React from 'react';
+// import { GiftedChat } from 'react-native-gifted-chat';
+// import {
+//   View,
+//   Button,
+//   StyleSheet,
+// } from 'react-native';
+
+// // import {
+// //   version, url, iam_apikey,
+// // } from 'react-native-dotenv';
+
+// // const AssistantV1 = require('ibm-watson/assistant/v1');
+
+// // const assistant = new AssistantV1({
+// //   version,
+// //   iam_apikey,
+// //   url,
+// // });
+
+// class ChatTab extends React.Component {
+//   state = {
+//     messages: [],
+//   }
+
+//   componentWillMount() {
+//     // console.log(assistant);
+//     this.setState({
+//       messages: [
+//         {
+//           _id: 0,
+//           text: 'Hi, I\'m Kahoot! Thanks for checking in today - what\'s your name?',
+//           createdAt: new Date(),
+
+//           user: {
+//             _id: 2,
+//             name: 'Kristie Chow',
+//           },
+//         },
+//         {
+//           _id: 2,
+//           text: 'Nice to meet you! How are you doing today?',
+//           createdAt: new Date(),
+//           user: {
+//             _id: 2,
+//             name: 'React Native',
+//           },
+//         },
+//         {
+//           _id: 4,
+//           text: 'I’m sorry to hear that, what’s wrong?',
+//           createdAt: new Date(),
+//           user: {
+//             _id: 2,
+//             name: 'React Native',
+//           },
+//         },
+//         {
+//           _id: 6,
+//           text: 'That’s tough. If you want to, you can tell me more about that, or we can talk about something else.',
+//           createdAt: new Date(),
+//           user: {
+//             _id: 2,
+//             name: 'React Native',
+//           },
+//         },
+//         {
+//           _id: 8,
+//           text: 'I can definitely understand why that is overwhelming for you. Is there anything else you want to share with me today?',
+//           createdAt: new Date(),
+//           user: {
+//             _id: 2,
+//             name: 'React Native',
+//           },
+//         },
+//         {
+//           _id: 10,
+//           text: 'Thanks for talking to me today. I hope you feel better soon and continue to come back to chat. In the meantime, you can always check on the history of your mood with me!',
+//           createdAt: new Date(),
+//           user: {
+//             _id: 2,
+//             name: 'React Native',
+//           },
+//         },
+//       ],
+//     });
+//   }
+
+//   onSend(messages = []) {
+//     this.setState(previousState => ({
+//       messages: GiftedChat.append(previousState.messages, messages),
+//     }));
+//     console.log(this.state.messages);
+//   }
+
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <View style={styles.buttonBox}>
+//           <Button title="Analysis" color="black" onPress={() => this.props.navigation.navigate('Reports')} />
+//         </View>
+//         <GiftedChat
+//           messages={this.state.messages}
+//           onSend={messages => this.onSend(messages)}
+//           user={{
+//             _id: 1,
+//           }}
+//         />
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'rgba(153, 164, 255, 0.5)',
+//     justifyContent: 'space-around',
+//   },
+//   buttonBox: {
+//     marginTop: 50,
+//     backgroundColor: '#CD8BE8',
+//     color: 'black',
+//     width: 200,
+//     height: 40,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     borderRadius: 10,
+//   },
+
+// });
+
+// export default ChatTab;
+
+// adapted from https://github.com/tonydiaz/wa-react-native-minimal/
 
 import React from 'react';
-import { GiftedChat } from 'react-native-gifted-chat';
 import {
   View,
   Button,
   StyleSheet,
 } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+// eslint-disable-next-line import/named
+import { MessageRequest } from './Assistant';
 
-// import {
-//   version, url, iam_apikey,
-// } from 'react-native-dotenv';
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-// const AssistantV1 = require('ibm-watson/assistant/v1');
-
-// const assistant = new AssistantV1({
-//   version,
-//   iam_apikey,
-//   url,
-// });
-
-class ChatTab extends React.Component {
-  state = {
-    messages: [],
+    this.state = {
+      messages: [],
+      context: null,
+    };
   }
 
-  componentWillMount() {
-    // console.log(assistant);
-    this.setState({
-      messages: [
-        {
-          _id: 0,
-          text: 'Hi, I\'m Kahoot! Thanks for checking in today - what\'s your name?',
-          createdAt: new Date(),
-
-          user: {
-            _id: 2,
-            name: 'Kristie Chow',
-          },
-        },
-        {
-          _id: 2,
-          text: 'Nice to meet you! How are you doing today?',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-          },
-        },
-        {
-          _id: 4,
-          text: 'I’m sorry to hear that, what’s wrong?',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-          },
-        },
-        {
-          _id: 6,
-          text: 'That’s tough. If you want to, you can tell me more about that, or we can talk about something else.',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-          },
-        },
-        {
-          _id: 8,
-          text: 'I can definitely understand why that is overwhelming for you. Is there anything else you want to share with me today?',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-          },
-        },
-        {
-          _id: 10,
-          text: 'Thanks for talking to me today. I hope you feel better soon and continue to come back to chat. In the meantime, you can always check on the history of your mood with me!',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'React Native',
-          },
-        },
-      ],
-    });
-  }
-
-  onSend(messages = []) {
-    this.setState(previousState => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }));
-    console.log(this.state.messages);
+  componentDidMount() {
+    this.initalMessage();
   }
 
   render() {
@@ -104,14 +172,67 @@ class ChatTab extends React.Component {
           <Button title="Analysis" color="black" onPress={() => this.props.navigation.navigate('Reports')} />
         </View>
         <GiftedChat
+          placeholder="Send your message to Watson..."
           messages={this.state.messages}
           onSend={messages => this.onSend(messages)}
+          multiline={false}
           user={{
-            _id: 1,
+            _id: '1',
           }}
         />
       </View>
     );
+  }
+
+  onSend = (message = []) => {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, message),
+    }), () => {
+      this.getMessage(message[0].text.replace(/[\n\r]+/g, ' '));
+    });
+  }
+
+  initalMessage = async () => {
+    const response = await MessageRequest('');
+    // console.log(response);
+    console.log(response._bodyInit.substring(35, 200));
+    console.log(response._bodyInit);
+    this.setState({
+      context: response.context,
+    });
+    const message = {
+      _id: Math.round(Math.random() * 1000000).toString(),
+      text: response._bodyInit.substring(101, 165),
+      createdAt: new Date(),
+      user: {
+        _id: '2',
+        name: 'Kahoot',
+      },
+    };
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, message),
+    }));
+  }
+
+  getMessage = async (text) => {
+    const response = await MessageRequest(text, this.state.context);
+    this.setState({
+      context: response.context,
+    });
+    console.log(response._bodyInit);
+    const startMessage = response._bodyInit.indexOf('"text","text":') + 15;
+    const message = {
+      _id: Math.round(Math.random() * 1000000).toString(),
+      text: response._bodyInit.substring(startMessage, startMessage + 71),
+      createdAt: new Date(),
+      user: {
+        _id: '2',
+        name: 'Kahoot',
+      },
+    };
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, message),
+    }));
   }
 }
 
@@ -131,7 +252,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderRadius: 10,
   },
-
 });
-
-export default ChatTab;
